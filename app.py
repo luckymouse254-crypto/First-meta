b073b3cfed81d42f539588feae22a51a2f81c352
luckymouse254-crypto <luckymouse254@gmail.com>
2026-09-01 06:18:26 +0300

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, jsonify
-import os
-
-app = Flask(_name_)
-
-@app.route('/')
-def home():
-    return "First Meta is Live 🚀 Builder: Andrea Kisumu Version: 0.2.3 FINAL FIX"
